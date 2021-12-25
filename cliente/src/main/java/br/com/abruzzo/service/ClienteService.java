59a67be02749bb28d6bbb8d09f41b69bb94d3dc6
Emmanuel de Oliveira D'Abruzzo <emmanuel.oliveira@trtsp.jus.br>
2021-12-25 15:55:51 -0300

    Elimina arquivos copiados por engano e adiciona anotação @service no ClienteService

--- cliente/src/main/java/br/com/abruzzo/service/ClienteService.java
+++ cliente/src/main/java/br/com/abruzzo/service/ClienteService.java
@@ -3,9 +3,11 @@ package br.com.abruzzo.service;
 import br.com.abruzzo.model.Cliente;
 import br.com.abruzzo.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Service
 public class ClienteService implements IClienteService {
 
         @Autowired
