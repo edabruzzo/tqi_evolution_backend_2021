decc741237bb302ed8b580ba09e5b63aa4534435
Emmanuel de Oliveira D'Abruzzo <emmanuel.oliveira@trtsp.jus.br>
2021-12-26 14:21:35 -0300

    Adiciona serviço de empréstimos e atualiza pom.xml do projeto emprestimo

--- emprestimo/src/main/java/EmprestimoApplication.java
+++ emprestimo/src/main/java/EmprestimoApplication.java
@@ -1,11 +1,14 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author Emmanuel Abruzzo
  * @date 25/12/2021
  */
+@SpringBootApplication
 public class EmprestimoApplication {
 
-
-
-
-
+    public static void main(String[] args) {
+        SpringApplication.run(EmprestimoApplication.class, args);
+    }
 }
