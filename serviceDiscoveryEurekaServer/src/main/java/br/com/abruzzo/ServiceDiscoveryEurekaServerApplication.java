4c6685c0edd327256466c1f977abe7729a458227
Emmanuel de Oliveira D'Abruzzo <emmanuel.oliveira@trtsp.jus.br>
2021-12-27 11:00:44 -0300

    Inicia configurações do Service Discovery Eureka

--- serviceDiscoveryEurekaServer/src/main/java/br/com/abruzzo/ServiceDiscoveryEurekaServerApplication.java
+++ serviceDiscoveryEurekaServer/src/main/java/br/com/abruzzo/ServiceDiscoveryEurekaServerApplication.java
@@ -2,8 +2,10 @@ package br.com.abruzzo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceDiscoveryEurekaServerApplication {
 
 	public static void main(String[] args) {
