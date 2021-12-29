76ecce7ee19901d0c156e2b352475159e664a517
Emmanuel de Oliveira D'Abruzzo <emmanuel.oliveira@trtsp.jus.br>
2021-12-29 11:25:09 -0300

    Cria configuração do Redis no Serviço de solicitação de empréstimos

--- servico_solicitacao_emprestimo/src/main/java/br/com/abruzzo/ServicoSolicitacaoEmprestimoApplication.java
+++ servico_solicitacao_emprestimo/src/main/java/br/com/abruzzo/ServicoSolicitacaoEmprestimoApplication.java
@@ -2,12 +2,33 @@ package br.com.abruzzo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
+@EnableRedisRepositories
 public class ServicoSolicitacaoEmprestimoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServicoSolicitacaoEmprestimoApplication.class, args);
 	}
+
+	@Bean
+	JedisConnectionFactory jedisConnectionFactory() {
+		JedisConnectionFactory jedisConFactory = new JedisConnectionFactory();
+		jedisConFactory.setHostName("localhost");
+		jedisConFactory.setPort(6379);
+		return jedisConFactory;
+	}
+
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate() {
+		RedisTemplate<String, Object> template = new RedisTemplate<>();
+		template.setConnectionFactory(jedisConnectionFactory());
+		return template;
+	}
+
 
 }
