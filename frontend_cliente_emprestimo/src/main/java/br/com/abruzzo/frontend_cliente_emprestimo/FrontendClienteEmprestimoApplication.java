7af2bbb2a11895abf184be125601b3ef60130a5c
Emmanuel de Oliveira D'Abruzzo <emmanuel.oliveira@trtsp.jus.br>
2022-01-01 20:25:53 -0300

    Adiciona bibliotecas de segurança ao projeto Frontend

--- frontend_cliente_emprestimo/src/main/java/br/com/abruzzo/frontend_cliente_emprestimo/FrontendClienteEmprestimoApplication.java
+++ frontend_cliente_emprestimo/src/main/java/br/com/abruzzo/frontend_cliente_emprestimo/FrontendClienteEmprestimoApplication.java
@@ -1,13 +1,39 @@
 package br.com.abruzzo.frontend_cliente_emprestimo;
 
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableFeignClients
 public class FrontendClienteEmprestimoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FrontendClienteEmprestimoApplication.class, args);
 	}
+
+
+
+	@Bean
+	public RequestInterceptor getRequestInterceptor(){
+		return new RequestInterceptor() {
+			@Override
+			public void apply(RequestTemplate requestTemplate) {
+				Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+				if(authentication==null)
+					return;
+
+				OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) authentication.getDetails();
+				requestTemplate.header("Authorization","Bearer"+details.getTokenValue());
+
+			}
+		}   ;
+	}
+
 
 }
--- frontend_cliente_emprestimo/src/main/java/br/com/abruzzo/frontend_cliente_emprestimo/client/ClienteFeignClient.java
+++ frontend_cliente_emprestimo/src/main/java/br/com/abruzzo/frontend_cliente_emprestimo/client/ClienteFeignClient.java
@@ -1,8 +1,11 @@
 package br.com.abruzzo.frontend_cliente_emprestimo.client;
 
 import br.com.abruzzo.frontend_cliente_emprestimo.dto.ClienteDTO;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 /**
  * @author Emmanuel Abruzzo
