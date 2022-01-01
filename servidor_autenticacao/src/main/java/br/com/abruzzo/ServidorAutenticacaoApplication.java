ba4489f699125fc856163f0d15036dd2ab7e2a6f
Emmanuel de Oliveira D'Abruzzo <emmanuel.oliveira@trtsp.jus.br>
2022-01-01 13:53:06 -0300

    Adiciona um servidor de autenticação

---
+++ servidor_autenticacao/src/main/java/br/com/abruzzo/ServidorAutenticacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.abruzzo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorAutenticacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorAutenticacaoApplication.class, args);
+	}
+
+}
