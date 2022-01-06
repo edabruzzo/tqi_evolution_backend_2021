958462efb970cfb13a027cbac675c176b6295667
Emmanuel de Oliveira D'Abruzzo <emmanuel.oliveira@trtsp.jus.br>
2022-01-06 19:50:56 -0300

    Ajusta configurações de autenticação

--- migration/cria_tb_users.sql
+++ migration/cria_tb_users.sql
@@ -1,16 +1,49 @@
 ﻿-- https://docs.spring.io/spring-security/reference/5.7/servlet/authentication/passwords/jdbc.html
 
+
+DROP TABLE IF EXISTS authorities;
+
 DROP table if exists users CASCADE;
 
-create table users(
-	username varchar(50) not null primary key,
-	password varchar(500) not null,
-	enabled boolean not null
+
+
+CREATE TABLE users (
+  username VARCHAR(50) NOT NULL,
+  password VARCHAR(100) NOT NULL,
+  enabled BOOLEAN NOT NULL DEFAULT TRUE,
+  PRIMARY KEY (username)
+);
+  
+CREATE TABLE authorities (
+  username VARCHAR(50) NOT NULL,
+  authority VARCHAR(50) NOT NULL,
+  FOREIGN KEY (username) REFERENCES users(username)
 );
 
-create table authorities (
-	username varchar(50) not null,
-	authority varchar(50) not null,
-	constraint fk_authorities_users foreign key(username) references users(username)
-);
-create unique index ix_auth_username on authorities (username,authority);
+CREATE UNIQUE INDEX ix_auth_username
+  on authorities (username,authority);
+
+
+/*
+
+SELECT * FROM users;
+SELECT * FROM authorities;
+SELECT * FROM usuario_roles;
+
+DELETE FROM authorities;
+DELETE FROM usuario_roles;
+DELETE FROM users;
+	
+
+insert into authorities
+select username,
+'ROLE_FUNCIONARIO'
+from users;
+
+
+insert into authorities
+select username,
+'ROLE_SUPER_ADMIN'
+from users;
+
+*/
